572ef07917766181c4934672083eb793275e0772
bsavyanov <team@omni-lab.app>
2026-09-23 11:50:50 +0300

    Add GreedyEgyptianDrobes project with initial implementation

---
+++ greedy_algorithms/GreedyEgyptianDrobes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+
+    static void Main()
+    {
+        Console.Clear();
+        string[] input = Console.ReadLine().Split();
+        int p = int.Parse(input[0]);
+        int q = int.Parse(input[1]);
+
+        while (p > 1)
+        {
+            int r = (q + p - 1) / p;
+
+            Console.Write($"1/{r} + ");
+
+            p = p * r - q;
+            q = q * r;
+
+            int d = GCD(p, q);
+            p /= d;
+            q /= d;
+        }
+
+        Console.WriteLine($"1/{q}");
+    }
+}
